229592ec6206a8d6240ededcc49de180344a4ef0
technologiessumud-commits <technologiessumud@gmail.com>
2026-04-09 22:15:29 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,40 +1,43 @@
-// firebase.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
-
-import { 
-  getAuth, 
-  createUserWithEmailAndPassword, 
-  signInWithEmailAndPassword 
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
 
-import { 
-  getFirestore, 
-  collection, 
-  addDoc, 
-  getDocs 
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  getDoc
 } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyDaDEbVpUUyLsmq5ilNp3CVLRs3ZX-ZWUM",
+  authDomain: "roomie-f3103.firebaseapp.com",
+  projectId: "roomie-f3103",
+  storageBucket: "roomie-f3103.firebasestorage.app",
+  messagingSenderId: "896474185176",
+  appId: "1:896474185176:web:0e4543682f594daf0a86ad"
 };
 
 const app = initializeApp(firebaseConfig);
 
-// SERVICES
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
-// EXPORT FUNCTIONS
 export {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
   collection,
   addDoc,
-  getDocs
+  getDocs,
+  doc,
+  getDoc
 };
