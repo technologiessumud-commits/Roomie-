7ff08f8accd6244957e06381db45b39bb61a3fed
technologiessumud-commits <technologiessumud@gmail.com>
2026-04-09 19:26:30 +0100

    Update firebase.js

---
+++ firebase.js
@@ -0,0 +1,16 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDaDEbVpUUyLsmq5ilNp3CVLRs3ZX-ZWUM",
+  authDomain: "roomie-f3103.firebaseapp.com",
+  projectId: "roomie-f3103",
+  storageBucket: "roomie-f3103.firebasestorage.app",
+  messagingSenderId: "896474185176",
+  appId: "1:896474185176:web:0e4543682f594daf0a86ad"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export { createUserWithEmailAndPassword, signInWithEmailAndPassword };
